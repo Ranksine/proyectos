1df842b45513d3f1deb09e038cf59347cbc1ce8e
Ranksine <aguilarcanteroomy@gmail.com>
2025-04-28 15:39:12 -0600

    Finalización del proyecto Gato
    Se concluyó con la práctica del ejercicio.
    Nota: Existe un bug en donde si una vez finalizada la partida se recarga la página, se habilita para realizar un turno más, en lugar de resetear el tablero y los turnos.

--- 02-gato/src/App.jsx
+++ 02-gato/src/App.jsx
@@ -1,42 +1,65 @@
 import { useState } from "react"
+import confetti from "canvas-confetti"
+import { Square } from "./components/Square.jsx"
+import {TURNS, WINNER_COMBOS} from "./components/constans.js"
+import { checkWinner, checkEndGame } from "./logic/board.js"
+import { WinnerModal } from "./components/WinnerModal.jsx"
+import { saveGameToStorage, resetGameStorage  } from "./logic/storage/index.js"
 
-const TURNS={
-  X:'x',
-  O:'o'
-}
-
-const Square = ({children, isSelected, updateBoard, index}) => {
-  const className = `square ${isSelected ? 'is-selected' : ''}` 
-  const handleClick = () => {
-    updateBoard(index)
-  }
-
-  return (
-    <div onClick={handleClick} className={className}>
-      {children}
-    </div>
-  )
-}
 
 function App() {
   // const [board, setBoard] = useState(['x','x','x','o','o','x','x','o','o'])
-  const [board, setBoard] = useState(Array(9).fill(null))
+  const [board, setBoard] = useState(()=>{
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  })
   
-  const [turn, setTurn] = useState(TURNS.X)
+  const [turn, setTurn] = useState(()=>{
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
+  const [winner, setWinner] = useState(null) // Null = No hay ganador, False = Empate 
+  
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+    
+    resetGameStorage()
+  }
+  
 
   const updateBoard = (index) => {
-    if (board[index]) return
+    // No actualizar si ya hay algo en esa posición
+    if (board[index]  || winner) return
     
+    // Actualizar el tablero
     const newBoard = [... board]
     newBoard[index] = turn
     setBoard(newBoard)
 
+    // Cambiar el turno
     const newTurn = turn ===TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+   
+    saveGameToStorage({
+      board:newBoard,
+      turn:newTurn
+    })
+    // Revisar si hay ganador
+    const newWinner = checkWinner(newBoard)
+    if(newWinner){
+      confetti()
+      setWinner(newWinner)
+    } else if(checkEndGame(newBoard)){
+      setWinner(false)
+    }
   }
 
   return (
     <main className="board">
+    <button onClick={resetGame}>Resetear el juego</button>
       <h1>Juego de Gato</h1>
 
       <section className="game">
@@ -59,6 +82,9 @@ function App() {
         <Square isSelected = {turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected = {turn === TURNS.O}>{TURNS.O}</Square>
       </section>
+
+      <WinnerModal resetGame={resetGame} winner={winner}/>
+
     </main>
 
 
